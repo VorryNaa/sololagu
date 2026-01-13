39d22b63a78f7f7b62bd9f430cf9f5ac5923f791
VorryNaa <tresgure033@gmail.com>
2026-01-13 19:19:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const audio = new Audio('https://files.catbox.moe/2bz695.mp3');
 audio.loop = true;
 audio.play();
+let audio = document.getElementById('bg-audio');
+let btn = document.getElementById('audio-btn');
+
+function toggleAudio() {
+  if (audio.paused) {
+    audio.play();
+    btn.innerHTML = 'Pause';
+  } else {
+    audio.pause();
+    btn.innerHTML = 'Play';
+  }
+}
